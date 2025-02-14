5dc9f8a49839c5d72932651cb0184049eaf57c26
Yu Chin Fabian Lim <flim@sg.ibm.com>
2025-02-14 06:34:26 +0000

    accuracy without anchor

--- src/open_r1/custom_tasks/limo.py
+++ src/open_r1/custom_tasks/limo.py
@@ -65,6 +65,49 @@ def accuracy_relaxed_reward(completions, solution, **kwargs):
 
     return rewards
 
+
+def accuracy_without_anchor_reward(completions, solution, **kwargs):
+    """Reward function that checks if the completion is the same as the ground truth."""
+
+    try:
+        contents = [completion[0]["content"] for completion in completions]
+    except:
+        contents = completions
+    rewards = []
+    for content, sol in zip(contents, solution):
+        gold_parsed = parse(sol, extraction_mode="first_match", extraction_config=[LatexExtractionConfig()])
+        if len(gold_parsed) != 0:
+            # We require the answer to be provided in correct latex (no malformed operators)
+            answer_parsed = parse(
+                content,
+                extraction_config=[
+                    LatexExtractionConfig(
+                        normalization_config=NormalizationConfig(
+                            nits=False,
+                            malformed_operators=False,
+                            basic_latex=True,
+                            equations=True,
+                            boxed='all',
+                            units=True,
+                        ),
+                        # Ensures that boxed is tried first
+                        boxed_match_priority=0,
+                        try_extract_without_anchor=True, # no anchor
+                    )
+                ],
+                extraction_mode="first_match",
+            )
+            # Reward 1 if the content is the same as the ground truth, 0 otherwise
+            reward = float(verify(answer_parsed, gold_parsed))
+        else:
+            # If the gold solution is not parseable, we reward 1 to skip this example
+            reward = 1.0
+            print("Failed to parse gold solution: ", sol)
+        rewards.append(reward)
+
+    return rewards
+
 def update_reward_funcs(registry, script_args):
     registry['format_fixed'] = format_fixed_reward 
     registry['accuracy_relaxed'] = accuracy_relaxed_reward 
+    registry['accuracy_without_anchor'] = accuracy_without_anchor_reward 
