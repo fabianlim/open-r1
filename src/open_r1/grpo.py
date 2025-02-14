11975e2125f54674e39748b540b1292cf714c0b5
Yu Chin Fabian Lim <flim@sg.ibm.com>
2025-02-14 02:56:06 +0000

    update with gpqa:diamond

--- src/open_r1/grpo.py
+++ src/open_r1/grpo.py
@@ -109,7 +109,7 @@ def accuracy_reward(completions, solution, **kwargs):
                             malformed_operators=False,
                             basic_latex=True,
                             equations=True,
-                            boxed=True,
+                            boxed='all',
                             units=True,
                         ),
                         # Ensures that boxed is tried first
@@ -206,7 +206,7 @@ def get_cosine_scaled_reward(
                             malformed_operators=False,
                             basic_latex=True,
                             equations=True,
-                            boxed=True,
+                            boxed='all',
                             units=True,
                         ),
                         boxed_match_priority=0,
