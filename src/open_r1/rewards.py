46c2d8a7b348b7ca1675965a7cf573b72a1e92be
Yu Chin Fabian Lim <flim@sg.ibm.com>
2025-05-23 18:26:27 -0400

    format boxed instead

--- src/open_r1/rewards.py
+++ src/open_r1/rewards.py
@@ -82,7 +82,7 @@ def accuracy_reward(completions: list[list[dict[str, str]]], solution: list[str]
 
 def format_reward(completions, **kwargs):
     """Reward function that checks if the reasoning process is enclosed within <think> and </think> tags, while the final answer is enclosed within <answer> and </answer> tags."""
-    pattern = r"^<think>\n.*?\n</think>\n<answer>\n.*?\n</answer>$"
+    pattern = r"^<think>\n.*?\n</think>.*\[\s*\\boxed\{.*\}\s*\\]?\s*$"
     completion_contents = [completion[0]["content"] for completion in completions]
     matches = [re.match(pattern, content, re.DOTALL | re.MULTILINE) for content in completion_contents]
     return [1.0 if match else 0.0 for match in matches]
